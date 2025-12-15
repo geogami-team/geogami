2d70820129203c9c90a9af50c8d819bf57482290
YouQam <qamazyousef@gmail.com>
2025-12-15 13:04:56 +0100

    feat: use uni server instead of Hetzner

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiURL: "https://api.geogami.ifgi.de",
+  apiURL: "https://api.geogami.uni-muenster.de",
   uiURL:"https://app.geogami.ifgi.de",
-  webglURL: "https://ve.geogami.ifgi.de",
-  dashboardURL: `https://dashboard.geogami.ifgi.de`,
+  webglURL: "https://ve.geogami.uni-muenster.de",
+  dashboardURL: `https://dashboard.geogami.uni-muenster.de`,
   mapboxAccessToken:
     "pk.eyJ1IjoiZmVsaXhhZXRlbSIsImEiOiI2MmE4YmQ4YjIzOTI2YjY3ZWFmNzUwOTU5NzliOTAxOCJ9.nshlehFGmK_6YmZarM2SHA",
   photoQuality: 10,
