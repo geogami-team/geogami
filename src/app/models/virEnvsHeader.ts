fc4dc4c4e876ab674ff2884f376f1a7bd7f8bf16
YouQam <qamazyousef@gmail.com>
2026-09-21 15:11:59 +0200

    feat(virEnvs): rename environment from "grid map_v1" to "grid map" in VirEnv_52

--- src/app/models/virEnvsHeader.ts
+++ src/app/models/virEnvsHeader.ts
@@ -255,7 +255,7 @@ export const VirEnvHeaders: ReadonlyArray<any> = [
     img_url: "assets/vir_envs_layers/VirEnv_51.png",
   },
   {
-    envName: "grid map_v1",
+    envName: "grid map",
     layerName: "VirEnv_52",
     img_url: "assets/vir_envs_layers/VirEnv_53.png",
   },
